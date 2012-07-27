eda0dc3c08d6a90ea509b108aa7419c50c6ec018
Dominik Liebler <liebler.dominik@googlemail.com>
2012-07-27 23:25:41 +0200

    refactored to use gen_server

--- src/statsd.erl
+++ src/statsd.erl
@@ -1,14 +1,18 @@
 -module(statsd).
 -author("Dominik Liebler").
+-behaviour(gen_server).
 
--export([start/2, start/1, start/0, stop/1]).
--export([increment/2, increment/3, decrement/2, decrement/3, count/3, count/4, timing/3, timing/4]).
+%% forced by gen_server
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-export([start/2, start/1, start/0, stop/0]).
+-export([increment/1, increment/2, decrement/1, decrement/2, count/2, count/3, timing/2, timing/3]).
 
 -define(STATSD_DEFAULT_PORT, 8125).
 -define(STATSD_DEFAULT_HOST, "localhost").
 
-%% holds all the relevant state that must be passed to all functions
--record(state, { 
+%% holds all the relevant state that is used internally to pass the socket around
+-record(state, {
 	port = ?STATSD_DEFAULT_PORT,
 	host = ?STATSD_DEFAULT_HOST,
 	socket
@@ -18,68 +22,89 @@
 %%
 %% returns a #state record containing the socket
 start(Host, Port) ->
-	State = #state{port = Port, host = Host},
 	{ok, Socket} = gen_udp:open(0),
-	State#state{socket = Socket}.
+	State = #state{port = Port, host = Host, socket = Socket},
+	gen_server:start_link({local, ?MODULE}, ?MODULE, [State], []).
 start(Host) ->
 	start(Host, ?STATSD_DEFAULT_PORT).
 start() ->
 	start(?STATSD_DEFAULT_HOST).
 
-%% Public: closes the socket connection
+%% Public: stop server
 %%
-%% returns ok (hopefully)
-stop(#state{socket=Socket}) ->
-	gen_udp:close(Socket).
+%% returns: {ok}
+stop() ->
+	gen_server:call(?MODULE, stop),
+	{ok}.
 
+%% Internal: used by gen_server and called on connection init
+%%
+%% returns: {ok, State}
+init([State]) ->
+	{ok, State}.
+
+%% Internal: used by gen_server and called on connection termination
+%%
+%% returns: {ok}
+terminate(_Reason, State) ->
+	{ok, State}.
+	
 %% Public: increments a counter by 1
 %% 
 %% returns ok or {error, Reason}
-increment(State, Key, Samplerate) when is_record(State, state) ->
-	count(State, Key, 1, Samplerate).
-increment(State, Key) when is_record(State, state) ->
-	count(State, Key, 1).
+increment(Key, Samplerate) ->
+	count(Key, 1, Samplerate).
+increment(Key) ->
+	count(Key, 1).
 	
 %% Public: decrements a counter by 1
 %% 
 %% returns ok or {error, Reason}
-decrement(State, Key, Samplerate) when is_record(State, state) ->
-	count(State, Key, -1, Samplerate).
-decrement(State, Key) when is_record(State, state) ->
-	count(State, Key, -1).
+decrement(Key, Samplerate) ->
+	count(Key, -1, Samplerate).
+decrement(Key) ->
+	count(Key, -1).
 
 %% Public: increments a counter by an arbitrary integer value
 %%
 %% returns: ok or {error, Reason}
-count(State, Key, Value) when is_record(State, state) ->
-	send(State, {message, Key, Value, c}).
-count(State, Key, Value, Samplerate) when is_record(State, state) ->
-	send(State, {message, Key, Value, c, Samplerate}).
+count(Key, Value) ->
+	send({message, Key, Value, c}).
+count(Key, Value, Samplerate) ->
+	send({message, Key, Value, c, Samplerate}).
 
 %% Public: sends a timing in ms
 %%
 %% returns: ok or {error, Reason}
-timing(State, Key, Value) when is_record(State, state) ->
-	send(State, {message, Key, Value, ms}).
-timing(State, Key, Value, Samplerate) when is_record(State, state) ->
-	send(State, {message, Key, Value, ms, Samplerate}).
+timing(Key, Value) ->
+	send({message, Key, Value, ms}).
+timing(Key, Value, Samplerate) ->
+	send({message, Key, Value, ms, Samplerate}).
 	
 %% Internal: prepares and sends the messages
 %%
 %% returns: ok or {error, Reason}
-send(State, Message) when is_record(State, state) ->
-	send_message(State, build_message(Message)).
+send(Message) ->
+	gen_server:call(?MODULE, {send_message, build_message(Message)}).
 
 %% Internal: builds the message string to be sent
 %% 
-%% returns: a String	
+%% returns: a String
 build_message({message, Key, Value, Type}) ->
 	lists:concat([Key, ":", Value, "|", Type]);
 build_message({message, Key, Value, Type, Samplerate}) ->
 	lists:concat([build_message({message, Key, Value, Type}) | ["@", io_lib:format("~.2f", [1.0 / Samplerate])]]).
-		
-%% Internal: sends the message over a UDP socket
-%% 
-%% returns: 
-send_message(State, Message) when is_record(State, state) ->
-	gen_udp:send(State#state.socket, State#state.host, State#state.port, Message).
+
+%% Internal: handles gen_server:call calls
+%%
+%% returns:	{reply, ok|error, State}
+handle_call({send_message, Message}, _From, State) ->
+	gen_udp:send(State#state.socket, State#state.host, State#state.port, Message),
+	{reply, ok, State};
+handle_call(stop, _From, State) ->
+	gen_udp:close(State#state.socket),
+	{stop, normal, stopped, State}.
+	
+code_change(_OldVersion, State, _Extra) -> {ok, State}.
+handle_cast(_Message, State) -> {ok, State}.
+handle_info(_Info, State) -> {ok, State}.
